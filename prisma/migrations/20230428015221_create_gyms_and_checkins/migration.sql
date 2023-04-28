9567157b46f9482d07f1023c6fb58ceb98a9841c
Larissa Rabelo <larissarabelolf@gmail.com>
2023-04-27 23:50:36 -0300

    feat: add migrations - creating users, gym, checkins

---
+++ prisma/migrations/20230428015221_create_gyms_and_checkins/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - Added the required column `password_hash` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "password_hash" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "check_ins" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "validate_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "gyms" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+    "address" TEXT,
+    "phone" TEXT,
+    "email" TEXT,
+    "website" TEXT,
+    "description" TEXT,
+    "image" TEXT,
+
+    CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
+);
